ed54285fbbfd428acf76bd15451b70dca335253a
polovy1o <polovy1o@gmail.com>
2024-01-31 16:18:07 +0200

    Merge step13. Add delete, create, update endpoints for hive sections (server)

--- artlogic-katla-server/controllers/ProductHiveSectionsController.js
+++ artlogic-katla-server/controllers/ProductHiveSectionsController.js
@@ -1,3 +1,4 @@
+import RequestedResourceHasConflictError from "../errors/RequestedResourceHasConflictError.js";
 import RequestedResourceNotFoundError from "../errors/RequestedResourceNotFoundError.js";
 import productHiveSectionsService from "../services/ProductHiveSectionsService.js"
 
@@ -19,6 +20,49 @@ class ProductHiveSectionsController {
             }
             res.json(hive)
         } catch (err) {
+            next(err)
+        }
+    }
+
+    async createHiveSection(req, res, next)
+    {
+        try {
+            await productHiveSectionsService.createHiveSection(req.body)
+            res.sendStatus(201)
+        } catch(err) {
+            next(err)
+        }
+    }
+
+    async deleteHiveSection(req, res, next)
+    {
+        try {
+            const id = req.params.sectionId;
+            const section = await productHiveSectionsService.getHiveSection(id)
+
+            if (!section) {
+                throw new RequestedResourceNotFoundError()
+            }
+            if (!section.isDeleted) {
+                throw new RequestedResourceHasConflictError()
+            }
+
+            await productHiveSectionsService.deleteHiveSection(id)
+            res.sendStatus(204)
+        } catch(err) {
+            next(err)
+        }
+    }
+
+    async updateHiveSection(req, res, next)
+    {
+        try {
+            const exists = await productHiveSectionsService.updateHiveSection(req.params.sectionId, req.body)
+            if (!exists) {
+                throw RequestedResourceNotFoundError()
+            }
+            res.sendStatus(204)
+        } catch(err) {
             next(err)
         }
     }
