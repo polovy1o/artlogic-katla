c4bd7ef7e4dc9de862f3ca2c61d37e081416517a
polovy1o <polovy1o@gmail.com>
2024-08-15 15:05:04 +0300

    Change main page link to API docs link

--- artlogic-katla-client/src/components/Main.jsx
+++ artlogic-katla-client/src/components/Main.jsx
@@ -6,8 +6,8 @@ function Main() {
             <h1 className="display-4"><span style={{ color: "blueviolet", fontWeight: "bold", fontFamily: "Arial, Helvetica, sans-serif", marginRight: "0.1em", border: "blueviolet solid 1px", padding: "6px", }}>K</span>KatlaSport</h1>
             <p className="lead">Welcome to KatlaSport management console.</p>
             <hr className="my-4"/>
-            <p>Please, click on "Learn more" button to get more information about this project.</p>
-            <a className="btn btn-primary btn-lg" target="_blank" href="https://github.com/polovy1o/artlogic-katla" role="button">Learn more</a>
+            <p>Please, click on "API docs" button to get more information about API that back-end provides.</p>
+            <a className="btn btn-primary btn-lg" target="_blank" href={process.env.REACT_APP_SERVER_URL + "/api-docs"} role="button">API docs</a>
         </div>
 
     )
