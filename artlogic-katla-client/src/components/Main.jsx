dd36e97e9559eeb094b419f84fc70de982497cee
polovy1o <polovy1o@gmail.com>
2024-08-15 14:57:26 +0300

    Some fixes

--- artlogic-katla-client/src/components/Main.jsx
+++ artlogic-katla-client/src/components/Main.jsx
@@ -7,11 +7,7 @@ function Main() {
             <p className="lead">Welcome to KatlaSport management console.</p>
             <hr className="my-4"/>
             <p>Please, click on "Learn more" button to get more information about this project.</p>
-<<<<<<< HEAD
             <a className="btn btn-primary btn-lg" target="_blank" href="https://github.com/polovy1o/artlogic-katla" role="button">Learn more</a>
-=======
-            <a className="btn btn-primary btn-lg" href="https://github.com/belpyro/artlogic-katla" role="button">Learn more</a>
->>>>>>> 433b13d30d21ccd676c55aea02f7eddc13983cc5
         </div>
 
     )
